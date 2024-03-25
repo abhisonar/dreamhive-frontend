3ffd0358730580eea9d28735b8d83a5d2d77e594
Abhishek Sonar <abhisonar4121@gmail.com>
2024-03-25 18:25:09 +0530

    nx initial setup and app creation

---
+++ apps/employer/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
+      ...createGlobPatternsForDependencies(__dirname)
+    ),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [require('../../tailwind-workspace-preset')],
+};
