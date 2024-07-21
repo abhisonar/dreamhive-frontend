08753ff358f71f4204a324019800e81a4ddec43d
Abhishek Sonar <abhisonar4121@gmail.com>
2024-07-22 01:14:07 +0530

    added angular material to dreamhive app

--- apps/dreamhive/src/app/app.config.ts
+++ apps/dreamhive/src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration, withNoHttpTransferCache } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withNoHttpTransferCache()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimationsAsync(),
   ],
 };
