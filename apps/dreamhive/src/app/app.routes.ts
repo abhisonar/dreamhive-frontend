6fdad9c7071da81f6b35fb96307cdbf01fee0980
Abhishek Sonar <abhisonar4121@gmail.com>
2024-07-22 00:28:23 +0530

    added login and registration component

--- apps/dreamhive/src/app/app.routes.ts
+++ apps/dreamhive/src/app/app.routes.ts
@@ -3,9 +3,15 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: '',
-    redirectTo: 'login'
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
     path: 'login',
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: 'registration',
+    loadComponent: () => import('./registration/registration.component').then(m => m.RegistrationComponent),
   }
 ];
